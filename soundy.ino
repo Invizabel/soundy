f7b9fef0983b2c1fc0cfc846d1233eae3050c42c
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-06-12 15:43:24 -0500

    Add files via upload

--- soundy.ino
+++ soundy.ino
@@ -1,10 +1,11 @@
 // SPDX-FileCopyrightText: 2023 Liz Clark for Adafruit Industries
 // SPDX-License-Identifier: MIT
 
-#define PIEZO_PIN  5 // Pin connected to the piezo buzzer.
+// Use D10 for CrunchLabs Arduino Kits
+#define PIEZO_PIN  10 // Pin connected to the piezo buzzer.
 
-int toneFreq = 440;
-int toneCount = 8;
+int toneFreq = 25000;
+int toneCount = 255;
 
 void setup()
 {
@@ -16,9 +17,9 @@ void loop()
   for (int i = 0; i < toneCount; i++)
   {
     tone(PIEZO_PIN, toneFreq);
-    delay(500);
-    noTone(PIEZO_PIN);
-    delay(500);
+    //delay(500);
+    //noTone(PIEZO_PIN);
+    //delay(500);
   }
   
   delay(5000);
